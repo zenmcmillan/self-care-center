b36fab5d38612f3e693324b51c68fb937f0c0e04
Zen McMillan <zenmcmillan@gmail.com>
2023-07-20 18:20:11 -0600

    Create functions to generate a random quote from the mantras or affirmations arrays

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 // query selectors
 
 var messageButton = document.querySelector('.receive-message-button');
-var affirmations = document.querySelector('input[]')
+var radioButtons = document.querySelectorAll('input[name="option"]');
+
+// event listeners
+messageButton.addEventListener('click', function () {
+
+});
+
+var currentQuote; //data model
+
+
+// functions
+
+
+
+//create function that
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function getRandomQuote(array) {
+  currentQuote = array[getRandomIndex(array)];
+  return currentQuote
+}
