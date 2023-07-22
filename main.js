b9e1085155ec12a8959b79f41f9a3359d42e244d
Zen McMillan <zenmcmillan@gmail.com>
2023-07-22 09:43:02 -0600

    refactored holdLoginPage function to not save an account objects unless they meet the requirements

--- main.js
+++ main.js
@@ -8,43 +8,42 @@ var loginContainer = document.querySelector('.second-container');
 var page = document.querySelectorAll('.page');
 var signInButton = document.querySelector('.sign-in-button');
 var loginFirstName = document.querySelector('.login-first-name');
-var loginLastName = document.querySelector('.login-last-name')
-var loginEmail = document.querySelector('.login-email')
-var loginPassword = document.querySelector('.login-password')
-var title = document.querySelector('.title')
+var loginLastName = document.querySelector('.login-last-name');
+var loginEmail = document.querySelector('.login-email');
+var loginPassword = document.querySelector('.login-password');
+var title = document.querySelector('.title');
 
 // data model
 
 var currentQuote;
 var currentAccount;
-var accounts = []
+var accounts = [];
 
 // event listeners
 
 window.addEventListener('load', function() {
-hideLoginPage() 
-this.setTimeout(showLoginPage, 1.6 * 1000)
+hideLoginPage();
+this.setTimeout(showLoginPage, 1.6 * 1000);
 });
 
 signInButton.addEventListener('click', function() {
- createAccount(loginFirstName.value, loginLastName.value, loginEmail.value, loginPassword.value) 
- holdLoginPage()
+ holdLoginPage();
 });
 
 // event handlers
+
 function holdLoginPage() {
-  if(loginFirstName.value.length && loginLastName.value.length && loginEmail.value.length && loginPassword.value.length > 8) {
-    removeLoginPage()
+  if (loginFirstName.value.length && loginLastName.value.length && loginEmail.value.length && loginPassword.value.length >= 8) {
+    createAccount(loginFirstName.value, loginLastName.value, loginEmail.value, loginPassword.value);
+    removeLoginPage();
+   } else {
+    window.alert('Sign up below.\n Make sure Password has at least 8 characters.');
   }
- else if (!loginFirstName.value.length && !loginLastName.value.length && !loginEmail.value.length && !loginPassword.value.length > 8) {
- 
- }
- window.alert('Sign up below.\n Make sure Password has at least 8 characters.')
-}
+};
 
 function removeLoginPage() {
   
-  loginContainer.setAttribute("id", "hidden") 
+  loginContainer.setAttribute("id", "hidden");
   for (var i = 0; i < page.length; i++) {
     page[i].classList.remove('hidden');
   }
@@ -54,7 +53,7 @@ function removeLoginPage() {
   `<h1 class="title">✨ Self Care Center ✨ 
   <p class="welcome" id="welcome-strong">Welcome ${currentAccount.firstName}</p>
   </h1>`     
-}
+};
 
 function createAccount(firstName, lastName, email, password) {
    currentAccount = {
@@ -62,26 +61,25 @@ function createAccount(firstName, lastName, email, password) {
   lastName: lastName,
   emailAddress: email,
   password: password,
-  }
-  accounts.push(currentAccount)
-  console.log(accounts)
-  return console.log(currentAccount)
-}
+  };
+  accounts.push(currentAccount);
+  return currentAccount
+};
 
 function showLoginPage() {
-  if (loginContainer.getAttribute("id") === "hidden")
-  loginContainer.removeAttribute("id")
+  if (loginContainer.getAttribute("id") === "hidden");
+  loginContainer.removeAttribute("id");
   for (var i = 0; i < page.length; i++) {
     page[i].classList.add('hidden');
   }
-}
+};
 
 function hideLoginPage() {
-  loginContainer.setAttribute("id", "hidden")
+  loginContainer.setAttribute("id", "hidden");
   for (var i = 0; i < page.length; i++) {
     page[i].classList.remove('hidden');
   }  
-}
+};
   
 messageButton.addEventListener('click', function() {
   chooseQuote();
@@ -100,28 +98,28 @@ function renderQuote(quote) {
   `<main>
    <p class="quote">${quote}</p>     
   </main>`
-}
+};
 
 function chooseQuote() {
   for (var i = 0; i < radioButtons.length; i++) {
     if (radioButtons[i].checked && radioButtons[i].value === 'affirmations') {
-       currentQuote = getRandomQuote(affirmations)
+       currentQuote = getRandomQuote(affirmations);
     }
     else if (radioButtons[i].checked && radioButtons[i].value === 'mantras') {
-       currentQuote = getRandomQuote(mantras)
-  }
+       currentQuote = getRandomQuote(mantras);
+     }
+   }
+   return currentQuote;
  }
- return console.log(currentQuote)
-}
 
 function getRandomQuote(array) {
-  var quote
-  quote = array[getRandomIndex(array)]
-  return quote
+  var quote;
+  quote = array[getRandomIndex(array)];
+  return quote;
 } 
 
 function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length)
+  return Math.floor(Math.random() * array.length);
 }
 
 
